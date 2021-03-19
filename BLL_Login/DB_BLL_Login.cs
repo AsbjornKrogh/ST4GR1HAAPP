ae42d3342d9715364431ff7ae1ded5ac98532a89
FrejaIBredahl <201904700@post.au.dk>
2021-03-19 13:49:18 +0100

    Create DTO objekt
    Create DTO objekt og opdateret indhold i DB_Login()

--- BLL_Login/DB_BLL_Login.cs
+++ BLL_Login/DB_BLL_Login.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Data.SqlClient;
+using System.Transactions;
 using DLL_Login;
 
 namespace BLL_Login
@@ -7,6 +8,26 @@ namespace BLL_Login
     
    public class DB_BLL_Login
    {
-        
-   }
+        private DB_Login DBLogin = new DB_Login();
+
+        //TODO kontoller DTO objekt for status char 
+
+        /// <summary>
+        /// Returns StaffStatus
+        /// </summary>
+        /// <param name="staffID"></param>
+        /// <param name="pw"></param>
+        /// <returns></returns>
+        public bool checkLogin(string staffID, string pw)
+       {
+           if (DBLogin.LoginStaff(staffID, pw) == true)
+           {
+               return true;
+           }
+           else
+           {
+               return false;
+           }
+       }
+    }
 }
