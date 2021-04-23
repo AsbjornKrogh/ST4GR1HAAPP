3d111f30de99acebf21b0b2f02161647e96c2f71
SivWestAlroe <201909204@post.au.dk>
2021-04-23 15:07:02 +0200

    manage

--- Presentation_Clinician/HearingTestWindow.xaml.cs
+++ Presentation_Clinician/HearingTestWindow.xaml.cs
@@ -7,6 +7,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
@@ -20,6 +21,7 @@ namespace Presentation_Clinician
         public HearingTestWindow()
         {
             InitializeComponent();
+            
         }
     }
 }
