7f3623e198dbf1cd44aaa1867447d6f8949be1d2
Ditte Bertelsen <201907172@uni.au.dk>
2021-04-30 14:47:25 +0200

    Påbegyndelse af UC6

--- Presentation_Technician/ProcessPage.xaml.cs
+++ Presentation_Technician/ProcessPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -10,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using CoreEFTest.Models;
+using DLL_Technician;
 
 namespace Presentation_Technician
 {
@@ -18,9 +21,36 @@ namespace Presentation_Technician
     /// </summary>
     public partial class ProcessPage : Page
     {
-        public ProcessPage()
+        private IClinicDB db;
+        private StaffLogin technician;
+        private bool okIsRunning;
+        public ProcessPage(IClinicDB db, StaffLogin login)
         {
             InitializeComponent();
+
+            this.db = db;
+            technician = login;
+        }
+
+        private void OKB_Click(object sender, RoutedEventArgs e)
+        {
+            if (okIsRunning != true)
+            {
+                okIsRunning = true;
+
+                BackgroundWorker worker = new BackgroundWorker();
+
+                worker.DoWork += UC4GetPatientInformation;
+
+                worker.RunWorkerCompleted += UC4GetPatientInformationCompleted;
+
+                string castID = HACastIDTB.Text;
+                worker.RunWorkerAsync(castID);
+
+                Loading.Visibility = Visibility.Visible;
+                Loading.Spin = true;
+            }
+
         }
     }
 }
