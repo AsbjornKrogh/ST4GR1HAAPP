4e161d13daf8799895ac4403d901a1064727687c
SivWestAlroe <201909204@post.au.dk>
2021-04-30 14:12:52 +0200

    HA

--- BBL_Clinician/UC3_ManageHA.cs
+++ BBL_Clinician/UC3_ManageHA.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using CoreEFTest.Models;
 using DLL_Clinician;
 
 namespace BBL_Clinician
@@ -12,23 +14,32 @@ namespace BBL_Clinician
            clinicDatabase = new ClinicDatabase();
        }
 
-       public void GetHearingTest(string CPRnummer)
+       public void GetHearingTest(string CPR)
        {
 
        }
 
-       public void GetHA(string CPRnummer)
+       public void GetHA(string CPR)
        {
-            
+           clinicDatabase.GetPatientWithEarCast(CPR);
        }
 
        public bool SaveHA(int HearingAidID, string type, string color, DateTime createDate, int ClinianID)
        {
+            
            return true;
            //måske skal vi benytte general spec her - ved ikke om man skal have en datetime og staffid med her
        }
 
-       public void GetAllHA(){}
+        public void CreateHA(GeneralSpec generalSpec)
+        {
+            
+        }
+
+        public List<GeneralSpec> GetAllHA(string CPR)
+       {
+          return clinicDatabase.GetAlleGeneralSpecs(CPR);
+       }
 
    }
 }
